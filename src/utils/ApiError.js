7fa5b5433b22c132364b878f558493261f12d58e
Rahul <rkkraj500@gmail.com>
2024-02-11 22:40:06 +0530

    utils set up

---
+++ src/utils/ApiError.js
@@ -0,0 +1,26 @@
+class apiError extends Error {
+    constructor(
+        statusCode,
+        message = "Something went worng",
+        errors= [],
+        statck = ""
+    ){
+        super(message)
+
+        this.statusCode = statusCode
+        this.message = message
+        this.data = null
+        this.errors = errors
+        this.success = false
+
+
+        if (statck) {
+            this.stack = statck
+        }else{
+            Error.captureStackTrace(this,this.constructor)
+        }
+    }
+}
+
+
+export { apiError }
